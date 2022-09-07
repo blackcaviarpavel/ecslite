bf3f70a227871cdc520eeabd8515c4b2cb23c7fc
Pavel Korobeinikov <p.korobeinikov@blackcaviar.games>
2022-09-07 21:06:49 +0500

    Change ecs world api

--- src/Worlds/EcsWorldExtensions.cs
+++ src/Worlds/EcsWorldExtensions.cs
@@ -21,7 +21,23 @@ namespace Submodules.EcsLite
 				pool.Add(entity);
 			}
 
-			return ref pool.Get(entity);
+			return ref pool.Change(entity);
+		}
+
+		public static void SetSingleEntity<T>(this EcsWorld ecsWorld, bool added) where T : struct
+		{
+			var entity = GetSingleEntity<T>(ecsWorld);
+			var pool = ecsWorld.GetPool<T>();
+			if (entity == EcsWorld.NullEntityIndex && added)
+			{
+				entity = ecsWorld.NewEntity();
+
+				pool.Add(entity);
+			}
+			else if (entity != EcsWorld.NullEntityIndex && !added)
+			{
+				pool.Remove(entity);
+			}
 		}
 
 		public static void RemoveSingleEntity<T>(this EcsWorld ecsWorld) where T : struct
